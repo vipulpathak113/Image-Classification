2699cd7555d127a968db25a6eca52f7f8a027cb3
Exploring-Ebay-Car-Sales-Data <vipulpathak113@gmail.com>
2020-10-03 02:19:41 +0530

    Updates

---
+++ precache-manifest.200c0fdc092a5c8152edebce2f41177c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b6ac07d501bed69751d9ae5fd52a97",
+    "url": "/Image-Classification/index.html"
+  },
+  {
+    "revision": "2361bd692fd03453044d",
+    "url": "/Image-Classification/static/css/main.a82ad031.chunk.css"
+  },
+  {
+    "revision": "892848a0b037211f0baa",
+    "url": "/Image-Classification/static/js/2.73aa3a28.chunk.js"
+  },
+  {
+    "revision": "94acd2dd16137b64cf94053598b7d526",
+    "url": "/Image-Classification/static/js/2.73aa3a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2361bd692fd03453044d",
+    "url": "/Image-Classification/static/js/main.ed76d93f.chunk.js"
+  },
+  {
+    "revision": "8d89fed0a1a72a3198d5",
+    "url": "/Image-Classification/static/js/runtime-main.647f179a.js"
+  }
+]);
